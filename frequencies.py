b41ba71cd38e238c69fcfd092d1437a2f2930c6f
fenn7 <afenn666@gmail.com>
2022-09-29 15:51:55 +0100

    frequently

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,25 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from operator import contains
+
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+    for item in items:
+        # convert items to strings
+        item = str(item)
+        
+        # updates the 'counter' for each unique item
+        if frequencies.get(item) == None:
+            scheduledUpdate = { item : 1 }
+        else:
+            scheduledUpdate = { item: int(frequencies.get(item) + 1) }  
+            
+        frequencies.update(scheduledUpdate)
+        
+    print (frequencies)
     return frequencies
+
+# frequencies( [1, "john", 1, 1, True, "john", "john", "1", True] )
